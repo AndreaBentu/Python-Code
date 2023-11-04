c4eb0e22a6e120cdd7d41e7f5f3980018ef54913
AndreaBentu <148153029+AndreaBentu@users.noreply.github.com>
2023-11-04 20:03:06 +0000

    Add files via upload

---
+++ Fundamentals/prime_numbers.py
@@ -0,0 +1,12 @@
+# list the prime numbers up to 10.
+
+for n in range(1,10):
+	if n==1:
+		print(f"{n} is a prime number.")
+	else:
+		for x in range(2,n):
+				if n % x == 0:
+					print(f"{n} equals {x} * {n/x}.")
+					break
+		else:
+			print(f"{n} is a prime number.")
