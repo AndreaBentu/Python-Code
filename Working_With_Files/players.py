934523fab21614a03f53a23c405e99676150469b
AndreaBentu <148153029+AndreaBentu@users.noreply.github.com>
2023-11-04 20:23:44 +0000

    Add files via upload

---
+++ Working_With_Files/players.py
@@ -0,0 +1,19 @@
+player1 = {
+    'name': 'Ignacio',
+    'numbers': {1, 2, 5, 9, 15}
+}
+player2 = {
+    'name': 'Ramona',
+    'numbers': {13, 21, 22, 4, 5}
+}
+
+lottery_numbers = {13, 21, 22, 5, 8}
+
+players = ['player1', 'player2']
+
+player1_numbers = lottery_numbers.intersection(player1['numbers'])
+player2_numbers = lottery_numbers.intersection(player2['numbers'])
+
+print( *player2_numbers, sep = ", ") 
+
+print( player1['name'] + ' has guessed the following numbers ' + str(player1_numbers)[1:-1] + ' and ' + player2['name'] +' has guessed the following numbers ' + str(player2_numbers)[1:-1] +'.')
